1abfc15e1445749b60ad548398466ddf7b49c8e6
ak-hayashi <eight.kayst@gmail.com>
2022-03-19 18:01:21 +0900

    [Add] Add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,42 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import { ChangeEvent } from "react";
 
 const Home: NextPage = () => {
-  return <h1>Hello World</h1>;
+  const [text, setText] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const onChangeText = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  const onClickTodos = () => {
+    const newTodos = [...todos];
+    newTodos.push(text);
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Todoを追加しよう</h1>
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => onChangeText(event)}
+        />
+        <button onClick={() => onClickTodos()}>追加</button>
+      </div>
+      <div>
+        <h1>Todoを確認しよう</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Home;
